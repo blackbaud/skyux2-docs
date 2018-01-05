705fe56e00dd68911b694915006a367f8df99405
Steve Brush <steve.brush@blackbaud.com>
2018-01-05 11:35:30 -0500

    First release (#3)

--- src/app/components/demo-components.module.ts
+++ src/app/components/demo-components.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 
+import { SkyDemoModule } from '@blackbaud/skyux/dist/demo';
+
 import { SkyDemoComponentsService } from './demo-components.service';
 
 require('style-loader!prismjs/themes/prism.css');
 
 @NgModule({
+  imports: [
+    SkyDemoModule
+  ],
+  exports: [
+    SkyDemoModule
+  ],
   providers: [
     SkyDemoComponentsService
   ]
