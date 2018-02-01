76f1c0fac1e59775e7e9252476c254cc42e7eb73
blackbaud-johnly <John.Lyons@blackbaud.com>
2018-01-31 21:01:53 -0500

    Clean up tab spacing and other inconsistencies.

--- src/app/components/toolbar/toolbar-guidelines.component.ts
+++ src/app/components/toolbar/toolbar-guidelines.component.ts
@@ -9,146 +9,147 @@ import { Component } from '@angular/core';
 export class SkyToolbarGuidelinesComponent {
 /* tslint:disable:max-line-length */
 public addButtonInfo: {label: string, value: string}[] = [
-    {
-      label: 'Text',
-      value: 'Add'
-    },
-    {
-      label: 'Icon',
-      value: 'fa-plus-circle'
-    },
-    {
-      label: 'Dropdown',
-      value: 'Optional. Allows users to add more than one type of record.'
-    },
-    {
-      label: 'Notes',
-      value: 'If add is the primary action, use the blue primary button styling. Otherwise, use the white secondary button styling.'
-    }
-  ];
+  {
+    label: 'Text',
+    value: 'Add'
+  },
+  {
+    label: 'Icon',
+    value: 'fa-plus-circle'
+  },
+  {
+    label: 'Dropdown',
+    value: 'Optional. Allows users to add more than one type of record.'
+  },
+  {
+    label: 'Notes',
+    value: 'If add is the primary action, use the blue primary button styling. Otherwise, use the white secondary button styling.'
+  }
+];
 public editButtonInfo: {label: string, value: string}[] = [
-    {
-      label: 'Text',
-      value: 'Edit'
-    },
-    {
-      label: 'Icon',
-      value: 'fa-pencil'
-    },
-    {
-      label: 'Dropdown',
-      value: 'Optional. Allows users to edit multiple types of records in a content container.'
-    }
-  ];
+  {
+    label: 'Text',
+    value: 'Edit'
+  },
+  {
+    label: 'Icon',
+    value: 'fa-pencil'
+  },
+  {
+    label: 'Dropdown',
+    value: 'Optional. Allows users to edit multiple types of records in a content container.'
+  }
+];
 public saveButtonInfo: {label: string, value: string}[] = [
-    {
-      label: 'Text',
-      value: 'Save'
-    },
-    {
-      label: 'Icon',
-      value: 'fa-save'
-    },
-    {
-      label: 'Dropdown',
-      value: 'Optional. Allows users to select multiple choices for how to save a list, such as "Save" and "Save as."'
-    }
-  ];
+  {
+    label: 'Text',
+    value: 'Save'
+  },
+  {
+    label: 'Icon',
+    value: 'fa-save'
+  },
+  {
+    label: 'Dropdown',
+    value: 'Optional. Allows users to select multiple choices for how to save a list, such as "Save" and "Save as."'
+  }
+];
 public filterButtonInfo: {label: string, value: string}[] = [
-    {
-      label: 'Text',
-      value: 'Filter'
-    },
-    {
-      label: 'Icon',
-      value: 'fa-filter'
-    },
-    {
-      label: 'Dropdown',
-      value: 'None'
-    }
-  ];
+  {
+    label: 'Text',
+    value: 'Filter'
+  },
+  {
+    label: 'Icon',
+    value: 'fa-filter'
+  },
+  {
+    label: 'Dropdown',
+    value: 'None'
+  }
+];
 public sortButtonInfo: {label: string, value: string}[] = [
-    {
-      label: 'Text',
-      value: 'Sort'
-    },
-    {
-      label: 'Icon',
-      value: 'fa-sort'
-    },
-    {
-      label: 'Dropdown',
-      value: 'None'
-    }
-  ];
+  {
+    label: 'Text',
+    value: 'Sort'
+  },
+  {
+    label: 'Icon',
+    value: 'fa-sort'
+  },
+  {
+    label: 'Dropdown',
+    value: 'None'
+  }
+];
 public columnsButtonInfo: {label: string, value: string}[] = [
-    {
-      label: 'Text',
-      value: 'Columns'
-    },
-    {
-      label: 'Icon',
-      value: 'fa-columns'
-    },
-    {
-      label: 'Dropdown',
-      value: 'None'
-    },
-    {
-      label: 'Notes',
-      value: 'Only visible when viewing a list as a grid.'
-    }
-  ];
+  {
+    label: 'Text',
+    value: 'Columns'
+  },
+  {
+    label: 'Icon',
+    value: 'fa-columns'
+  },
+  {
+    label: 'Dropdown',
+    value: 'None'
+  },
+  {
+    label: 'Notes',
+    value: 'Only visible when viewing a list as a grid.'
+  }
+];
 public shareButtonInfo: {label: string, value: string}[] = [
-    {
-      label: 'Text',
-      value: 'Share'
-    },
-    {
-      label: 'Icon',
-      value: 'fa-share'
-    },
-    {
-      label: 'Dropdown',
-      value: 'None'
-    }
-  ];
+  {
+    label: 'Text',
+    value: 'Share'
+  },
+  {
+    label: 'Icon',
+    value: 'fa-share'
+  },
+  {
+    label: 'Dropdown',
+    value: 'None'
+  }
+];
 public exportButtonInfo: {label: string, value: string}[] = [
-    {
-      label: 'Text',
-      value: 'Export'
-    },
-    {
-      label: 'Icon',
-      value: 'fa-file-o, fa-file-excel-o, or fa-file-pdf-o'
-    },
-    {
-      label: 'Dropdown',
-      value: 'Optional. Allows users to select from multiple export options.'
-    },
-    {
-      label: 'Notes',
-      value: 'For multiple export options, use the generic fa-file-o icon and a dropdown. For a single export option, use the icon for the file type that the option creates.'
-    }
-  ];
+  {
+    label: 'Text',
+    value: 'Export'
+  },
+  {
+    label: 'Icon',
+    value: 'fa-file-o, fa-file-excel-o, or fa-file-pdf-o'
+  },
+  {
+    label: 'Dropdown',
+    value: 'Optional. Allows users to select from multiple export options.'
+  },
+  {
+    label: 'Notes',
+    value: 'For multiple export options, use the generic fa-file-o icon and a dropdown. For a single export option, use the icon for the file type that the option creates.'
+  }
+];
 public moreButtonInfo: {label: string, value: string}[] = [
-    {
-      label: 'Text',
-      value: 'More'
-    },
-    {
-      label: 'Icon',
-      value: 'fa-ellipsis-h'
-    },
-    {
-      label: 'Dropdown',
-      value: 'None'
-    },
-    {
-      label: 'Notes',
-      value: 'Only use the more actions button for two or more items. For a single action, display the action button in the toolbar.' }
-  ];
+  {
+    label: 'Text',
+    value: 'More'
+  },
+  {
+    label: 'Icon',
+    value: 'fa-ellipsis-h'
+  },
+  {
+    label: 'Dropdown',
+    value: 'None'
+  },
+  {
+    label: 'Notes',
+    value: 'Only use the more actions button for two or more items. For a single action, display the action button in the toolbar.'
+  }
+];
 public findBoxInfo: {label: string, value: string}[] = [
   {
     label: 'Text',
