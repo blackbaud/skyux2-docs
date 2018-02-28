8e6e022945048dc2b024591108d7a259924c76d7
Blackbaud-ToddRoberts <todd.roberts@blackbaud.com>
2018-02-28 14:03:15 -0500

    flyout ux guidelines

---
+++ src/app/components/flyout/flyout-guidelines.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sky-flyout-guidelines',
+  templateUrl: './flyout-guidelines.component.html',
+  styleUrls: ['./flyout-guidelines.component.scss']
+})
+
+export class SkyFlyoutGuidelinesComponent {}
