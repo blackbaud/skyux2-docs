6c78dd5da5fd188ed197b9f4cdabc7ecaed6daa6
Blackbaud-ToddRoberts <todd.roberts@blackbaud.com>
2018-05-14 12:35:30 -0400

    start

--- src/app/design/guidelines/guideline-action-button.component.ts
+++ src/app/design/guidelines/guideline-action-button.component.ts
@@ -62,6 +62,13 @@ export class GuidelineActionButtonsComponent {
       // tslint:disable-next-line
       summary: 'Responsive, mobile-first layouts provide flexibility and conform to SKY UX standards.'
     },
+		{
+      name: 'Spacing & layout',
+      path: '/design/guidelines/spacing-layout',
+      icon: 'th-large',
+      // tslint:disable-next-line
+      summary: 'Spacing guidelines facilitate a clean page layout.'
+    },
     {
       name: 'User assistance',
       path: '/design/guidelines/user-assistance',
