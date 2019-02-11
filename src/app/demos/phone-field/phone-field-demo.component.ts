7d7086bc1a16216ab355c6272fc9ce17eeefab78
BLACKBAUD\Trevor.Burch <Trevor.Burch@blackbaud.com>
2019-02-11 11:52:44 -0500

    Updated demo to be more descriptive and helpful

--- src/app/demos/phone-field/phone-field-demo.component.ts
+++ src/app/demos/phone-field/phone-field-demo.component.ts
@@ -1,17 +1,32 @@
 import {
-  Component
+  Component,
+  OnInit
 } from '@angular/core';
+
+import {
+  FormGroup,
+  FormControl
+} from '@angular/forms';
 
 @Component({
   selector: 'sky-phone-field-demo',
   templateUrl: './phone-field-demo.component.html'
 })
-export class SkyPhoneFieldDemoComponent {
+export class SkyPhoneFieldDemoComponent implements OnInit {
 
   public phoneNumber: string;
 
-  public phoneNumber2: string;
+  public phoneForm: FormGroup;
+
+  public phoneControl: FormControl;
 
   constructor() { }
+
+  public ngOnInit() {
+    this.phoneControl = new FormControl();
+    this.phoneForm = new FormGroup({
+      'phoneControl': this.phoneControl
+    });
+  }
 
 }
