ea1214ae5121c7aacaa920654c46a9a6343024bd
Alex Kingman <alex.kingman@blackbaud.com>
2019-04-22 10:19:09 -0400

    i dont know

--- src/app/demos/inline-form/inline-form-demo.component.ts
+++ src/app/demos/inline-form/inline-form-demo.component.ts
@@ -28,6 +28,29 @@ export class SkyInlineFormDemoComponent {
     { id: '2', name: 'Raised', amount: '$25.00' }
   ];
 
+  public repeaterItems = [
+    {
+      id: '1',
+      title: '2018 Gala',
+      note: '2018 Gala for friends and family',
+      fund: 'General 2018 Fund'
+    },
+    {
+      id: '2',
+      title: '2018 Special event',
+      note: 'Special event',
+      fund: '2018 Special Events Fund'
+    },
+    {
+      id: '3',
+      title: '2019 Gala',
+      note: '2019 Gala for friends and family',
+      fund: 'General 2019 Fund'
+    }
+  ];
+
+  public activeInlineFormId: number;
+
   public editConfig: SkyInlineFormConfig = {
     buttonLayout: SkyInlineFormButtonLayout.SaveDeleteCancel
   };
@@ -52,5 +75,12 @@ export class SkyInlineFormDemoComponent {
   public onClosePageTitleForm(event: SkyInlineFormCloseArgs): void {
     this.showPageTitleForm = false;
     console.log(event);
+  }
+
+  public onInlineFormClose(inlineFormCloseArgs: SkyInlineFormCloseArgs): void {
+    console.log(inlineFormCloseArgs);
+    this.activeInlineFormId = undefined;
+
+    // Form handling would go here
   }
 }
