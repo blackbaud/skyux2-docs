a761999c26d7ab62d4a982acb6be86a0ad980787
blackbaud-johnly <John.Lyons@blackbaud.com>
2019-04-24 08:44:44 -0400

    Tweak tile header and event description

--- src/app/demos/inline-form/inline-form-demo-tile.component.ts
+++ src/app/demos/inline-form/inline-form-demo-tile.component.ts
@@ -14,7 +14,7 @@ export class SkyInlineFormDemoTileComponent {
   public showEventDescriptionForm = false;
 
   // tslint:disable-next-line:max-line-length
-  public eventDescription: string = `Spring is here and it's time to celebrate the strawberry. Join us for the 9th annual Spring Strawberry Festival at Sunny Park with some of your favorite music and vendors. Bring the family with activities for all age groups - from face painting to strawberry eating.`;
+  public eventDescription: string = `Spring is here, and it's time to celebrate the strawberry. Join us for the 9th annual Spring Strawberry Festival at Sunny Park with some of your favorite music and vendors. Bring the family to enjoy activities for all age groups — from face painting to strawberry eating.`;
 
   public onInlineFormClose(inlineFormCloseArgs: SkyInlineFormCloseArgs): void {
     console.log(inlineFormCloseArgs);
