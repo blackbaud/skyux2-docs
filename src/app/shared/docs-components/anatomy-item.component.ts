feff63a5e952b4f9fad073438dba93bd0ce406dc
Blackbaud-ToddRoberts <todd.roberts@blackbaud.com>
2019-05-02 15:44:58 -0400

    anatomy files

---
+++ src/app/shared/docs-components/anatomy-item.component.ts
@@ -0,0 +1,12 @@
+import {
+  Component
+} from '@angular/core';
+
+@Component({
+  selector: 'sky-anatomy-item',
+  templateUrl: './anatomy-item.component.html',
+  styleUrls: ['./anatomy-item.component.scss']
+})
+export class AnatomyItemComponent {
+
+}
