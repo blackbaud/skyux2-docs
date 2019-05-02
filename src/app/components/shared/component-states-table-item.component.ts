08b52325906d5cce1515ba475a0cce186bf3a11b
Blackbaud-ToddRoberts <todd.roberts@blackbaud.com>
2019-05-02 17:12:36 -0400

    states table files

---
+++ src/app/components/shared/component-states-table-item.component.ts
@@ -0,0 +1,12 @@
+import {
+  Component
+} from '@angular/core';
+
+@Component({
+  selector: 'sky-docs-component-states-table-item',
+  templateUrl: './component-states-table-item.component.html',
+  styleUrls: ['./component-states-table-item.component.scss']
+})
+export class ComponentStatesTableItemComponent {
+
+}
---
+++ src/app/components/shared/component-states-table.component.ts
@@ -0,0 +1,13 @@
+import {
+  Component,
+  Input
+} from '@angular/core';
+
+@Component({
+  selector: 'sky-docs-component-states-table',
+  templateUrl: './component-states-table.component.html',
+  styleUrls: ['./component-states-table.component.scss']
+})
+export class ComponentStatesTableComponent {
+
+}
