85ed1cecfafd011f06108235262d3baf61fc4dcd
Blackbaud-ToddRoberts <todd.roberts@blackbaud.com>
2019-05-02 16:48:18 -0400

    anatomy component

--- src/app/shared/docs-components/anatomy-item.component.ts
+++ src/app/shared/docs-components/anatomy-item.component.ts
@@ -1,12 +1,17 @@
 import {
-  Component
+  Component,
+  Input
 } from '@angular/core';
 
 @Component({
-  selector: 'sky-anatomy-item',
+  selector: 'sky-docs-anatomy-item',
   templateUrl: './anatomy-item.component.html',
   styleUrls: ['./anatomy-item.component.scss']
 })
 export class AnatomyItemComponent {
 
+  @Input() isOptional: boolean;
+  
+  @Input() anatomyNumber: string;
+  
 }
