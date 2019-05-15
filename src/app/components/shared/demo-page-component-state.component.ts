c2d1743c51751608d8ed6e5e71966688ebf3ab22
Blackbaud-ToddRoberts <todd.roberts@blackbaud.com>
2019-05-15 11:07:23 -0400

    rename component states component

--- src/app/components/shared/demo-page-component-state.component.ts
+++ src/app/components/shared/demo-page-component-state.component.ts
@@ -4,9 +4,9 @@ import {
 } from '@angular/core';
 
 @Component({
-  selector: 'sky-demo-page-states-table-item',
-  templateUrl: './demo-page-states-table-item.component.html',
-  styleUrls: ['./demo-page-states-table-item.component.scss']
+  selector: 'sky-demo-page-component-state',
+  templateUrl: './demo-page-component-state.component.html',
+  styleUrls: ['./demo-page-component-state.component.scss']
 })
 export class ComponentStatesTableItemComponent {
   @Input() public stateName: string;
