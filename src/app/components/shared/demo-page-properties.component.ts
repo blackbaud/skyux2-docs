fd47c214d2ca9a322a216ebfbc518ae49e0803ed
Blackbaud-ToddRoberts <todd.roberts@blackbaud.com>
2019-06-03 16:32:29 -0400

    Redo tab & popover pages (#377)
    * start tab page
    
    * more tabs guidelines
    
    * more tabs guidelines
    
    * states
    
    * ux layout content
    
    * add sidebar
    
    * page template
    
    * move module + install to dev tab
    
    * property tables v1
    
    * update table style
    
    * responsive property table (#390)
    
    * popover page structure
    
    * popover content
    
    * more popover content
    
    * styles
    
    * styles + images
    
    * testing 2x images
    
    * retry
    
    * lint
    
    * lint
    
    * update responsiveness example
    
    * lint
    
    * lint
    
    * another dont guideline
    
    * renaming content components
    
    * more renaming
    
    * module component
    
    * property table cleanup
    
    * property cleanup
    
    * anatomy files
    
    * anatomy component
    
    * lint
    
    * lint
    
    * states table files
    
    * state table progress
    
    * state table styling
    
    * update styles
    
    * anatomy imports
    
    * more input types
    
    * lint
    
    * renaming state table component
    
    * style cleanup
    
    * moving styles around
    
    * Doc tweaks to updated popover and tabs docs (#439)
    
    * Doc tweaks to updated popover and tabs docs
    
    * Doc tweak
    
    * Remove extra header
    
    * layout
    
    * edits
    
    * property table updates
    
    * tabs -> spaces
    
    * rename component states component
    
    * anatomy component
    
    * tab anatomy update
    
    * rename two-col component
    
    * added gifs
    
    * added gifs to page
    
    * removing popover animations
    
    * add sidebar to old component page template
    
    * update component pages to sidebar

--- src/app/components/shared/demo-page-properties.component.ts
+++ src/app/components/shared/demo-page-properties.component.ts
@@ -7,6 +7,7 @@ import {
 @Component({
   selector: 'sky-demo-page-properties',
   templateUrl: './demo-page-properties.component.html',
+  styleUrls: ['./demo-page-properties.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SkyDemoPagePropertiesComponent {
