314cab37b10a40ea785fed848ba8af4e07fa8c11
Denise Pena <denise.pena@blackbaud.me>
2019-06-05 13:40:58 -0500

    Merged master into branch

---
+++ src/app/shared/docs-components/visual-example.component.ts
@@ -0,0 +1,12 @@
+import {
+  Component
+} from '@angular/core';
+
+@Component({
+  selector: 'sky-docs-visual-example',
+  templateUrl: './visual-example.component.html',
+  styleUrls: ['./visual-example.component.scss']
+})
+export class TwoColRightImageComponent {
+
+}
