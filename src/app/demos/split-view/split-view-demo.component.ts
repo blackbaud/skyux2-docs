e7635631bf80ff78de4f4e4fd831c18838763d0b
Alex Kingman <alex.kingman@blackbaud.com>
2019-07-09 17:19:10 -0400

    modified inputs to match latest codebase. updated demo to match UX guidelines

--- src/app/demos/split-view/split-view-demo.component.ts
+++ src/app/demos/split-view/split-view-demo.component.ts
@@ -14,10 +14,13 @@ import {
   Subject
 } from 'rxjs/Subject';
 
-import { SkySplitViewMessage } from '@skyux/split-view/modules/split-view/types/split-view-message';
-import { SkySplitViewMessageType } from '@skyux/split-view/modules/split-view/types/split-view-message-type';
-import { SkySplitViewBeforeWorkspaceCloseHandler }
-from '@skyux/split-view/modules/split-view/types/split-view-before-workspace-close-handler';
+import {
+  SkySplitViewMessage
+} from '@skyux/split-view/modules/split-view/types/split-view-message';
+
+import {
+  SkySplitViewMessageType
+} from '@skyux/split-view/modules/split-view/types/split-view-message-type';
 
 @Component({
   selector: 'sky-split-view-demo',
@@ -33,19 +36,47 @@ export class SkySplitViewDemoComponent implements AfterViewInit {
   public listWidth: number;
 
   public items = [
-    { id: 1, name: 'Jennifer Standley', amount: 12.45, date: '04/28/2019' },
-    { id: 2, name: 'Jennifer Standley', amount: 52.39, date: '04/22/2019' },
-    { id: 3, name: 'Jennifer Standley', amount: 9.12, date: '04/09/2019' },
-    { id: 4, name: 'Jennifer Standley', amount: 193.00, date: '03/27/2019' },
-    { id: 5, name: 'Jennifer Standley', amount: 19.89, date: '03/11/2019' },
-    { id: 6, name: 'Jennifer Standley', amount: 214.18, date: '02/17/2019' },
-    { id: 7, name: 'Jennifer Standley', amount: 4.53, date: '02/26/2019' }
+    {
+      id: 1,
+      amount: 73.19,
+      date: '5/13/2019',
+      vendor: 'amazon.com',
+      receiptImage: 'amzn-office-supply-order-5-13-19.png',
+      approvedAmount: 73.19,
+      comments: ''
+    },
+    {
+      id: 2,
+      amount: 214.12,
+      date: '5/14/2019',
+      vendor: 'Office Max',
+      receiptImage: 'office-max-order.png',
+      approvedAmount: 214.12,
+      comments: ''
+    },
+    {
+      id: 3,
+      amount: 29.99,
+      date: '5/14/2019',
+      vendor: 'amazon.com',
+      receiptImage: 'amzn-office-supply-order-5-14-19.png',
+      approvedAmount: 29.99,
+      comments: ''
+    },
+    {
+      id: 4,
+      amount: 1500,
+      date: '5/15/2019',
+      vendor: 'Fresh Catering, LLC',
+      receiptImage: 'fresh-catering-llc-order.png',
+      approvedAmount: 1500,
+      comments: ''
+    }
   ];
 
   public set activeIndex(value: number) {
     this._activeIndex = value;
     this.activeRecord = this.items[this._activeIndex];
-    this.checkForDisableIterators();
   }
 
   public get activeIndex(): number {
@@ -61,47 +92,25 @@ export class SkySplitViewDemoComponent implements AfterViewInit {
     public changeDetectorRef: ChangeDetectorRef
   ) {
     this.activeIndex = 0;
-
-    // This avoids a ExpressionChangedAfterItHasBeenCheckedError.
-    setTimeout(() => {
-      this.checkForDisableIterators();
-    });
   }
 
   public ngAfterViewInit(): void {
   }
 
   public onItemClick(index: number) {
-    this.activeIndex = index;
-    this.setFocusInWorkspace();
-  }
-
-  public onIteratorNextButtonClick(): void {
-    if (this.activeIndex < this.items.length - 1) {
-      this.activeIndex++;
-      this.setFocusInWorkspace();
-    }
-  }
-
-  public onIteratorPreviousButtonClick(): void {
-    if (this.activeIndex > 0) {
-      this.activeIndex--;
-      this.setFocusInWorkspace();
-    }
-  }
-
-  public onBeforeWorkspaceClose(closeHandler: SkySplitViewBeforeWorkspaceCloseHandler): void {
-    if (this.hasUnsavedWork) {
+    if (this.hasUnsavedWork && index !== this.activeIndex) {
       this.confirmService.open({
-        message: 'You have unsaved work. Are you sure you want to close this dialog?',
+        message: 'You have unsaved work. Are you sure you want to load a new item and lose your work?',
         type: SkyConfirmType.YesCancel
       }).closed.subscribe((closeArgs: SkyConfirmCloseEventArgs) => {
         if (closeArgs.action.toLowerCase() === 'yes') {
-          closeHandler.closeWorkspace();
+          this.activeIndex = index;
+          this.setFocusInWorkspace();
         }
       });
     } else {
-      closeHandler.closeWorkspace();
+      this.activeIndex = index;
+      this.setFocusInWorkspace();
     }
   }
 
@@ -110,26 +119,6 @@ export class SkySplitViewDemoComponent implements AfterViewInit {
       type: SkySplitViewMessageType.FocusWorkspace
     };
     this.splitViewStream.next(message);
-  }
-
-  private sendMessage(type: SkySplitViewMessageType) {
-    const message: SkySplitViewMessage = {
-      type: type
-    };
-    this.splitViewStream.next(message);
-  }
-
-  private checkForDisableIterators(): void {
-    if (this.activeIndex === this.items.length - 1) {
-      this.sendMessage(SkySplitViewMessageType.DisableIteratorNextButton);
-    } else {
-      this.sendMessage(SkySplitViewMessageType.EnableIteratorNextButton);
-    }
-    if (this.activeIndex === 0) {
-      this.sendMessage(SkySplitViewMessageType.DisableIteratorPreviousButton);
-    } else {
-      this.sendMessage(SkySplitViewMessageType.EnableIteratorPreviousButton);
-    }
   }
 
 }
