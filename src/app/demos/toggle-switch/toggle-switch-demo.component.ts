8ce8693b8416929c827ba7b50498cc19b6c5d9e7
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2019-07-24 10:43:33 -0400

    Added demo code

--- src/app/demos/toggle-switch/toggle-switch-demo.component.ts
+++ src/app/demos/toggle-switch/toggle-switch-demo.component.ts
@@ -2,8 +2,26 @@ import {
   Component
 } from '@angular/core';
 
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup
+} from '@angular/forms';
+
 @Component({
   selector: 'sky-toggle-switch-demo',
   templateUrl: './toggle-switch-demo.component.html'
 })
-export class SkyToggleSwitchDemoComponent { }
+export class SkyToggleSwitchDemoComponent {
+
+  public formGroup: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) {
+    this.formGroup = this.formBuilder.group({
+      notifyByEmail: new FormControl(true)
+    });
+  }
+
+}
