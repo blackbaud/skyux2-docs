4ba13fd14dce8b7901a4fa0569f5981b7b5557cc
Blackbaud-AdamFunderburk <adam.funderburk@blackbaud.com>
2019-10-15 12:20:14 -0400

    Character count design docs (#647)
    * Design documentation for new "character count" component
    
    * Update index.html
    
    * Update character-count-state-4.png
    
    * Update index.html
    
    * Update character-count-state-1.png
    
    * character count demo and technical docs
    
    * add textareas to demo
    
    * update forms package to include fixed char count styling
    
    * iteration after feedback
    
    * Character count docs edits (#687)
    
    * update demo to more realistic example
    
    * fix code files section

--- src/app/components/demo-components.service.ts
+++ src/app/components/demo-components.service.ts
@@ -109,6 +109,18 @@ export class SkyDemoComponentsService {
         getCodeFiles: () => this.getDemoFiles('Card')
       },
       {
+        name: 'Character count indicator',
+        icon: 'calculator',
+        summary: `The character count indicator component extends a text input to apply a character limit and display an indicator.`,
+        url: '/components/character-count',
+        imports: {
+          '@skyux/forms': [
+            'SkyCharacterCounterModule'
+          ]
+        },
+        getCodeFiles: () => this.getDemoFiles('Character count')
+      },
+      {
         name: 'Checkbox',
         icon: 'check-square',
         summary: `The checkbox component renders a SKY UX-themed checkbox.`,
