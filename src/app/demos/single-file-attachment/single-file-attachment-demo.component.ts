d85199f188b5aa07dc619df6c14c88e3f0d65d6a
Erika McVey <erika.mcvey@blackbaud.com>
2019-10-21 14:44:30 -0400

    update demo to show upload errors

--- src/app/demos/single-file-attachment/single-file-attachment-demo.component.ts
+++ src/app/demos/single-file-attachment/single-file-attachment-demo.component.ts
@@ -1,4 +1,5 @@
 import {
+  AbstractControl,
   FormBuilder,
   FormGroup,
   FormControl,
@@ -11,6 +12,7 @@ import {
 } from '@angular/core';
 
 import {
+  SkyFileAttachmentChange,
   SkyFileItem
 } from '@skyux/forms';
 
@@ -27,13 +29,21 @@ export class SkySingleFileAttachmentDemoComponent implements OnInit {
 
   public fileValue: SkyFileItem;
 
-  public maxFileSize: number = 4000000;
+  public reactiveUploadError: string;
+
+  public templateUploadError: string;
+
+  public maxFileSize: number = 500000;
 
   public showLabel: boolean = true;
 
   constructor(
     private formBuilder: FormBuilder
   ) { }
+
+  public get reactiveFile(): AbstractControl {
+    return this.fileForm.get('attachment');
+  }
 
   public ngOnInit(): void {
     this.attachment = new FormControl(undefined, Validators.required);
@@ -42,11 +52,47 @@ export class SkySingleFileAttachmentDemoComponent implements OnInit {
     });
   }
 
+  public reactiveFileUpdated(result: SkyFileAttachmentChange): void {
+    const file = result.file;
+
+    if (file && file.errorType) {
+      this.reactiveFile.setValue(undefined);
+      this.reactiveUploadError = this.getErrorMessage(file.errorType, file.errorParam);
+
+    } else {
+      this.reactiveFile.setValue(file);
+      this.reactiveUploadError = undefined;
+    }
+  }
+
+  public templateFileUpdated(result: SkyFileAttachmentChange): void {
+    const file = result.file;
+
+    if (file && file.errorType) {
+      this.fileValue = undefined;
+      this.templateUploadError = this.getErrorMessage(file.errorType, file.errorParam);
+
+    } else {
+      this.fileValue = file;
+      this.templateUploadError = undefined;
+    }
+  }
+
   public validateFile(file: SkyFileItem): string {
     if (file.file.name.indexOf('a') === 0) {
-        return 'You may not upload a file that begins with the letter "a."';
+      return 'You may not upload a file that begins with the letter "a."';
     } else {
       return '';
+    }
+  }
+
+  private getErrorMessage(errorType: string, errorParam: string): string {
+    if (errorType === 'fileType') {
+      return `Please upload a file of type ${errorParam}.`;
+    } else if (errorType === 'maxFileSize') {
+      return `Please upload a file smaller than ${errorParam} KB.`;
+    } else {
+      return errorParam;
     }
   }
 }
