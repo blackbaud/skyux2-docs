0708d854656f3acee9bb4012dae329a56e4bb9c4
Blackbaud-ToddRoberts <todd.roberts@blackbaud.com>
2020-06-18 16:35:43 -0400

    set up restricted view

--- src/app/app-extras.module.ts
+++ src/app/app-extras.module.ts
@@ -1,40 +1,34 @@
 import {
   NgModule
 } from '@angular/core';
-
 import {
   StacheModule
 } from'@blackbaud/skyux-lib-stache';
-
+import {
+  SkyRestrictedViewModule
+} from '@blackbaud/skyux-lib-restricted-view';
 import {
   SkyDemoComponentsModule
 } from './components/demo-components.module';
-
 import {
   SkyInlineFormDemoTileComponent
 } from './demos/inline-form/inline-form-demo-tile.component';
-
 import {
   SkyDemoSidebarService
 } from './shared/sidebar.service';
-
 import {
   SkyDemoTitleService
 } from './shared/title.service';
-
 require('style-loader!./styles.scss');
-
 import {
   SkyDocsAnatomyModule,
   SkyDocsDemoPageModule,
   SkyDocsDesignGuidelinesModule,
   SkyDocsSupportalService
 } from '@skyux/docs-tools';
-
 import {
   AppSkyModule
 } from './app-sky.module';
-
 @NgModule({
   entryComponents: [
     SkyInlineFormDemoTileComponent
@@ -49,6 +43,7 @@ import {
     SkyDocsAnatomyModule,
     SkyDocsDemoPageModule,
     SkyDocsDesignGuidelinesModule,
+    SkyRestrictedViewModule,
     StacheModule
   ],
   providers: [
